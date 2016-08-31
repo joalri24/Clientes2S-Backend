44d21f4e16ab08012db82dfe9e82802893905525
Jose Riaño <joalri24@hotmail.com>
2016-08-31 15:16:40 -0500

    Agregados los controladores

---
+++ Clientes2S-Backend/Clientes2S-Backend/Models/Clientes2S_BackendContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Clientes2S_Backend.Models
+{
+    public class Clientes2S_BackendContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Clientes2S_BackendContext() : base("name=Clientes2S_BackendContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Clientes2S_Backend.Models.Client> Clients { get; set; }
+
+        public System.Data.Entity.DbSet<Clientes2S_Backend.Models.Job> Jobs { get; set; }
+
+        public System.Data.Entity.DbSet<Clientes2S_Backend.Models.Contact> Contacts { get; set; }
+    }
+}
